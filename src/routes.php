14351e710779e866b14ab65e6b4b99dc9b7a68b8
Administrator <Administrator@ADMIN.domenfo.com>
2023-09-26 11:35:46 +0300

    Настройка роутинга

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,15 @@
 
 use App\Controllers\IndexController;
 
-//require_once 'app/controllers/IndexController.php';
-
 $route = explode("/", $_SERVER['REQUEST_URI']);     
 $route = str_replace(".php", "", $route);
 
 //echo "<pre>";
 //var_dump($route);
 
-$route = new IndexController;
-$route->index();
+//Запускаем разбор адресной строки
+if($route[2] == "index" || $route[2] == ""){
+    $route = new IndexController;
+    $route->index();
+}
+
