f42562a42a471e5ae773fbf66f626b69fd3dace2
Administrator <Administrator@ADMIN.domenfo.com>
2023-10-02 12:19:02 +0300

    Правка стартового шаблона

--- views/start.php
+++ views/start.php
@@ -78,7 +78,7 @@ $date = [
 	<link rel="stylesheet" href="assets/css/dark-theme.css" />
 	<link rel="stylesheet" href="assets/css/semi-dark.css" />
 	<link rel="stylesheet" href="assets/css/header-colors.css" />
-	<title>Исполнение бюджета Костомукша</title>
+	<title>Бюджет для граждан</title>
 </head>
 
 <body>
