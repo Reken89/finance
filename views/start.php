e8d2028f1293ec6ad0f64b417370858fd6857c99
Administrator <Administrator@ADMIN.domenfo.com>
2023-10-17 10:38:07 +0300

    Оформление кода

--- views/start.php
+++ views/start.php
@@ -17,7 +17,7 @@ $date = [
 ];
 
 ?>
-<!-- Значения для отрисовки диаграммы "Кассовый план" -->
+<!-- Передаем переменные из PHP в JS -->
 <div
     class='hidden'
     data-janr='<?= $data['diagram']['janr'] ?>'
@@ -73,152 +73,145 @@ $date = [
 </head>
 
 <body>
-	<!--wrapper-->
-	<div class="wrapper">
-	 <!--start header wrapper-->	
-	  <div class="header-wrapper">
+    <!--wrapper-->
+    <div class="wrapper">
+        <!--start header wrapper-->	
+        <div class="header-wrapper">
             <?php include("views/header.html") ?>
-	   </div>
-	   <!--end header wrapper-->
-		<!--start page wrapper -->
-		<div class="page-wrapper">
-			<div class="page-content">
-				<div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
-                   <div class="col">
-					 <div class="card radius-10 border-start border-0 border-3 border-success">
-						<div class="card-body">
-							<div class="d-flex align-items-center">
-								<div>
-									<p class="mb-0 text-secondary">Доходы на <?php echo $date[$data['block']['income']['mounth']]; ?></p>
-									<h4 class="my-1 text-success"><?php echo number_format($data['block']['income']['income'], 1, ',', ' '); ?> Тыс/Руб</h4>
-									<p class="mb-0 font-13"><?php echo $data['block']['income']['percent'] ?>% от годового плана</p>
-								</div>
-								<div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto">
-								</div>
-							</div>
-						</div>
-					 </div>
-				   </div>
-				   <div class="col">
-					<div class="card radius-10 border-start border-0 border-3 border-danger">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Расходы на <?php echo $date[$data['block']['expenses']['mounth']]; ?></p>
-								   <h4 class="my-1 text-danger"><?php echo number_format($data['block']['expenses']['expenses'], 1, ',', ' '); ?> Тыс/Руб</h4>
-								   <p class="mb-0 font-13"><?php echo $data['block']['expenses']['percent'] ?>% от годового плана</p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto">
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div>
-				  <div class="col">
-					<div class="card radius-10 border-start border-0 border-3 border-info">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Дефицит/Профицит на <?php echo $date[$data['block']['income']['mounth']]; ?></p>
-								   <h4 class="my-1 text-info"><?php echo number_format($data['block']['three']['three'], 1, ',', ' '); ?> Тыс/Руб</h4>
-                                                                   <p class="mb-0 font-13"><?php if($data['block']['three']['three'] > 0){echo "Профицит";}else{echo "Дефицит";} ?></p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto">
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div>
-				  <div class="col">
-					<div class="card radius-10 border-start border-0 border-3 border-warning">
-					   <div class="card-body">
-						   <div class="d-flex align-items-center">
-							   <div>
-								   <p class="mb-0 text-secondary">Муниципальный долг на <?php echo $date[$data['fourblock']['mounth']]; ?></p>
-								   <h4 class="my-1 text-warning"><?php echo number_format($data['fourblock']['mundeb'], 1, ',', ' '); ?> Тыс/Руб</h4>
-								   <p class="mb-0 font-13"><?php echo number_format($data['fourblock']['servmundeb'], 1, ',', ' '); ?> обслуживание мун.долга (%)</p>
-							   </div>
-							   <div class="widgets-icons-2 rounded-circle bg-gradient-blooker text-white ms-auto">
-							   </div>
-						   </div>
-					   </div>
-					</div>
-				  </div> 
-				</div><!--end row-->
-
-				<div class="row">
-                   <div class="col-12 col-lg-8">
-                      <div class="card radius-10">
-						  <div class="card-body">
-							<div class="d-flex align-items-center">
-								<div>
-									<h6 class="mb-0">Кассовый план на <?php echo $date[$data['diagram']['mounth']]; ?></h6>
-								</div>
-
-							</div>
-							<div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #008000"></i>Доходы</span>
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #CD5C5C"></i>Расходы</span>
-							</div>
-							<div class="chart-container-1">
-								<canvas id="chart1"></canvas>
-							  </div>
-						  </div>
-						  
-					  </div>
-				   </div>
-				   <div class="col-12 col-lg-4">
-                      <div class="card radius-10">
-						  <div class="card-body">
-							<div class="d-flex align-items-center">
-								<div>
-									<h6 class="mb-0">Исполнение по доходам и расходам на <?php echo $date[$data['block']['income']['mounth']]; ?></h6>
-								</div>
-								
-							</div>
-							<div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #00008B"></i>Утверждено</span>
-								<span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #800080"></i>Исполнено</span>
-							</div>
-							<div class="chart-container-1">
-								<canvas id="chart6"></canvas>
-							  </div>
-						  </div>
-					  </div>
-				   </div>
-				</div><!--end row-->
-                                
-                                <div class="row">
-                                    <div class="col-12 col-lg-6">
-                                        <div class="card radius-10">
-                                            <div class="card-body">
-                                                <div id="chart15"></div>
-                                            </div>
-                                        </div>
+        </div>
+        <!--end header wrapper-->
+        <!--start page wrapper -->
+        <div class="page-wrapper">
+            <div class="page-content">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+                    <div class="col">
+                        <div class="card radius-10 border-start border-0 border-3 border-success">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <p class="mb-0 text-secondary">Доходы на <?php echo $date[$data['block']['income']['mounth']]; ?></p>
+                                        <h4 class="my-1 text-success"><?php echo number_format($data['block']['income']['income'], 1, ',', ' '); ?> Тыс/Руб</h4>
+                                        <p class="mb-0 font-13"><?php echo $data['block']['income']['percent'] ?>% от годового плана</p>
                                     </div>
-                                    
-                                    <div class="col-12 col-lg-6">
-                                        <div class="card radius-10">
-                                            <div class="card-body">
-                                                <div id="chart16"></div>
-                                            </div>
-                                        </div>
+                                    <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto">
                                     </div>
-                                </div><!--end row-->				
-			</div>
-		</div>
-		
-		<footer class="page-footer">
-		    <?php include("views/footer.html") ?>
-		</footer>
-	</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card radius-10 border-start border-0 border-3 border-danger">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <p class="mb-0 text-secondary">Расходы на <?php echo $date[$data['block']['expenses']['mounth']]; ?></p>
+                                        <h4 class="my-1 text-danger"><?php echo number_format($data['block']['expenses']['expenses'], 1, ',', ' '); ?> Тыс/Руб</h4>
+                                        <p class="mb-0 font-13"><?php echo $data['block']['expenses']['percent'] ?>% от годового плана</p>
+                                    </div>
+                                    <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card radius-10 border-start border-0 border-3 border-info">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <p class="mb-0 text-secondary">Дефицит/Профицит на <?php echo $date[$data['block']['income']['mounth']]; ?></p>
+                                        <h4 class="my-1 text-info"><?php echo number_format($data['block']['three']['three'], 1, ',', ' '); ?> Тыс/Руб</h4>
+                                        <p class="mb-0 font-13"><?php if($data['block']['three']['three'] > 0){echo "Профицит";}else{echo "Дефицит";} ?></p>
+                                    </div>
+                                        <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto">
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card radius-10 border-start border-0 border-3 border-warning">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <p class="mb-0 text-secondary">Муниципальный долг на <?php echo $date[$data['fourblock']['mounth']]; ?></p>
+                                        <h4 class="my-1 text-warning"><?php echo number_format($data['fourblock']['mundeb'], 1, ',', ' '); ?> Тыс/Руб</h4>
+                                        <p class="mb-0 font-13"><?php echo number_format($data['fourblock']['servmundeb'], 1, ',', ' '); ?> обслуживание мун.долга (%)</p>
+                                    </div>
+                                        <div class="widgets-icons-2 rounded-circle bg-gradient-blooker text-white ms-auto">
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </div><!--end row-->
+
+                <div class="row">
+                    <div class="col-12 col-lg-8">
+                        <div class="card radius-10">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <h6 class="mb-0">Кассовый план на <?php echo $date[$data['diagram']['mounth']]; ?></h6>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
+                                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #008000"></i>Доходы</span>
+                                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #CD5C5C"></i>Расходы</span>
+                                </div>
+                                <div class="chart-container-1">
+                                    <canvas id="chart1"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="card radius-10">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <h6 class="mb-0">Исполнение по доходам и расходам на <?php echo $date[$data['block']['income']['mounth']]; ?></h6>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center ms-auto font-13 gap-2 my-3">
+                                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #00008B"></i>Утверждено</span>
+                                    <span class="border px-1 rounded cursor-pointer"><i class="bx bxs-circle me-1" style="color: #800080"></i>Исполнено</span>
+                                </div>
+                                <div class="chart-container-1">
+                                    <canvas id="chart6"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end row-->
+
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                        <div class="card radius-10">
+                            <div class="card-body">
+                                <div id="chart15"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <div class="card radius-10">
+                            <div class="card-body">
+                                <div id="chart16"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--end row-->				
+            </div>
+        </div>
+        <footer class="page-footer">
+            <?php include("views/footer.html") ?>
+        </footer>
+    </div>
 	<!--end wrapper-->
-
 <div>    
     <?php include("views/script.html") ?>
 </div>
     
 </body>
-
 </html>
 
